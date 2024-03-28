7ca02e9117116c2ed9d5d18f8ac59f7cab79320a
Florian HENRY <florian.henry@open-concept.pro>
2024-03-28 22:01:39 +0100

    fix copy some fields

--- core/triggers/interface_99_modRevertInvoice_RevertInvoiceTriggers.class.php
+++ core/triggers/interface_99_modRevertInvoice_RevertInvoiceTriggers.class.php
@@ -273,10 +273,11 @@ class InterfaceRevertInvoiceTriggers extends DolibarrTriggers
 									$tmpsupplierinvoice->fk_project = $object->fk_project;
 									$tmpsupplierinvoice->note_private = $object->note_private;
 									$tmpsupplierinvoice->note_public = $object->note_public;
-									$tmpsupplierinvoice->cond_reglement_id = $object->cond_reglement_id;
 									$tmpsupplierinvoice->mode_reglement_id = $object->mode_reglement_id;
-                                    $tmpsupplierinvoice->date_echeance = $object->date_lim_reglement;
-                                    $tmpsupplierinvoice->array_options = $object->array_options;
+									$tmpsupplierinvoice->date_echeance = $object->date_lim_reglement;
+									$tmpsupplierinvoice->cond_reglement_id = $object->cond_reglement_id;
+									$tmpsupplierinvoice->fk_account = $object->fk_account;
+									$tmpsupplierinvoice->array_options['options_wrike_project'] = $object->array_options['options_wrike_project'];
 
 									$tmpsupplierinvoice->lines = $object->lines;
 
